b7a94c1633c5352e8625f9dd4133b1f44fdd6d2b
Luigi Labigalini <26639149+labigalini@users.noreply.github.com>
2024-09-06 15:07:52 -0700

    Do not shrink when 0 messages

--- components/chat/chat-main.tsx
+++ components/chat/chat-main.tsx
@@ -36,65 +36,61 @@ export function ChatMain() {
   return (
     <div className="flex h-full w-full flex-col justify-between">
       <ChatTopbar name={chat.name} image={chat.image} />
-      <div className="flex h-full w-full flex-col overflow-y-auto">
-        <ChatMessageList ref={messagesContainerRef}>
-          <AnimatePresence>
-            {messages.map((message, index) => {
-              const variant = message.isViewer ? "sent" : "received";
-              return (
-                <motion.div
-                  key={message._id}
-                  layout
-                  initial={{
-                    opacity: 0,
-                    scale: 1,
-                    y: 50,
-                    x: 0,
-                  }}
-                  animate={{
-                    opacity: 1,
-                    scale: 1,
-                    y: 0,
-                    x: 0,
-                  }}
-                  exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
-                  transition={{
-                    opacity: { duration: 0.1 },
-                    layout: {
-                      type: "spring",
-                      bounce: 0.3,
-                      duration: index * 0.05 + 0.2,
-                    },
-                  }}
-                  style={{ originX: 0.5, originY: 0.5 }}
-                  className="flex flex-col gap-2 p-4"
-                >
-                  <ChatBubble variant={variant}>
-                    <ChatAvatar
-                      name={message.author.name}
-                      avatar={message.author.image}
-                    />
-                    <ChatBubbleMessage
+      <ChatMessageList ref={messagesContainerRef}>
+        <AnimatePresence>
+          {messages.map((message, index) => {
+            const variant = message.isViewer ? "sent" : "received";
+            return (
+              <motion.div
+                key={message._id}
+                layout
+                initial={{
+                  opacity: 0,
+                  scale: 1,
+                  y: 50,
+                  x: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                  scale: 1,
+                  y: 0,
+                  x: 0,
+                }}
+                exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
+                transition={{
+                  opacity: { duration: 0.1 },
+                  layout: {
+                    type: "spring",
+                    bounce: 0.3,
+                    duration: index * 0.05 + 0.2,
+                  },
+                }}
+                style={{ originX: 0.5, originY: 0.5 }}
+                className="flex flex-col gap-2 p-4"
+              >
+                <ChatBubble variant={variant}>
+                  <ChatAvatar
+                    name={message.author.name}
+                    avatar={message.author.image}
+                  />
+                  <ChatBubbleMessage
+                    variant={variant}
+                    // isLoading={message.isLoading} // TODO add support to typing...
+                  >
+                    {message.text}
+                    <ChatBubbleTimestamp timestamp={message._creationTime} />
+                    <ChatBubbleCountdown
                       variant={variant}
-                      // isLoading={message.isLoading} // TODO add support to typing...
-                    >
-                      {message.text}
-                      <ChatBubbleTimestamp timestamp={message._creationTime} />
-                      <ChatBubbleCountdown
-                        variant={variant}
-                        timestamp={message._expirationTime}
-                        duration={
-                          message._expirationTime - message._creationTime
-                        }
-                      />
-                    </ChatBubbleMessage>
-                  </ChatBubble>
-                </motion.div>
-              );
-            })}
-          </AnimatePresence>
-        </ChatMessageList>
-      </div>
+                      timestamp={message._expirationTime}
+                      duration={message._expirationTime - message._creationTime}
+                    />
+                  </ChatBubbleMessage>
+                </ChatBubble>
+              </motion.div>
+            );
+          })}
+        </AnimatePresence>
+      </ChatMessageList>
       <ChatBottombar />
     </div>
   );
