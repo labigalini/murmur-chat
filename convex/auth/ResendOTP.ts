47ca7862d348d81870ffc6e04c151a3dc24bf8ce
Luigi Labigalini <26639149+labigalini@users.noreply.github.com>
2024-09-05 21:37:11 -0700

    Email 'to' override env var

--- convex/auth/ResendOTP.ts
+++ convex/auth/ResendOTP.ts
@@ -6,19 +6,21 @@ export const ResendOTP = Email({
   id: "resend-otp",
   apiKey: process.env.AUTH_RESEND_KEY,
   maxAge: 60 * 15, // 15 minutes
-  async generateVerificationToken() {
+  generateVerificationToken() {
     return generateRandomString(8, alphabet("0-9"));
   },
   async sendVerificationRequest({ identifier: email, provider, token }) {
     const resend = new ResendAPI(provider.apiKey);
+    const toEmail = process.env.DEV_EMAIL ?? email;
     const { error } = await resend.emails.send({
       from: process.env.AUTH_EMAIL ?? "My App <onboarding@resend.dev>",
-      to: [email],
+      to: process.env.DEV_EMAIL ?? email,
       subject: `Sign in to My App`,
       text: "Your code is " + token,
     });
     if (error) {
-      throw new Error("Failed to send OTP email.");
+      console.error(error, toEmail);
+      throw new Error(`Failed to send OTP email.`);
     }
   },
 });
