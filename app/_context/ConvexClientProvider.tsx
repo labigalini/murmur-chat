9e4732581024f4265daad7cf2008ea02104e1517
Luigi Labigalini <26639149+labigalini@users.noreply.github.com>
2024-09-09 07:18:38 -0700

    Clean up convex provider

--- app/_context/ConvexClientProvider.tsx
+++ app/_context/ConvexClientProvider.tsx
@@ -3,22 +3,17 @@
 import { ReactNode } from "react";
 
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-
-import { useMediaQuery } from "usehooks-ts";
+import { ConvexReactClient } from "convex/react";
 
 import { ErrorBoundary } from "@/components/helpers/ErrorBoundary";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  const _prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   return (
     <ErrorBoundary>
       <ConvexAuthProvider client={convex}>
-        <ConvexProvider client={convex}>
-          <>{children}</>
-        </ConvexProvider>
+        <>{children}</>
       </ConvexAuthProvider>
     </ErrorBoundary>
   );
