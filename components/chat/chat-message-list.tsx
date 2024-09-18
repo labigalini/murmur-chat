88c0ef092b6352e1a7e5af602a6d0be39723800b
Luigi Labigalini <26639149+labigalini@users.noreply.github.com>
2024-09-17 21:00:43 -0700

    Update major version of dependencies

--- components/chat/chat-message-list.tsx
+++ components/chat/chat-message-list.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-interface ChatMessageListProps extends React.HTMLAttributes<HTMLDivElement> {}
+type ChatMessageListProps = React.HTMLAttributes<HTMLDivElement>;
 
 const ChatMessageList = React.forwardRef<HTMLDivElement, ChatMessageListProps>(
   ({ className, children, ...props }, ref) => (
