264e0c9a51eabb07c2126e9e054ee37ad9a0a245
Luigi Labigalini <26639149+labigalini@users.noreply.github.com>
2024-09-28 00:03:10 -0700

    Remove unused cookie from chat page

--- app/[chatId]/page.tsx
+++ app/[chatId]/page.tsx
@@ -1,14 +1,6 @@
-import { cookies } from "next/headers";
-
 import ChatBoard from "./_components/ChatBoard";
 
 export default function Page({ params }: { params: { chatId?: string } }) {
   const { chatId } = params;
-
-  const layout = cookies().get("react-resizable-panels:layout");
-  const defaultLayout = layout
-    ? (JSON.parse(layout.value) as number[])
-    : undefined;
-
   return <ChatBoard selectedChatId={chatId} />;
 }
