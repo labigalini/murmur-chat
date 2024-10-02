3ebbbabe95489486978837c49a5353ac49868adc
Luigi Labigalini <26639149+labigalini@users.noreply.github.com>
2024-10-02 00:15:43 -0700

    Add constant for RESEND_DEV_EMAIL

--- convex/auth/ResendOTP.ts
+++ convex/auth/ResendOTP.ts
@@ -3,6 +3,8 @@ import { Email } from "@convex-dev/auth/providers/Email";
 import { alphabet, generateRandomString } from "oslo/crypto";
 import { Resend as ResendAPI } from "resend";
 
+import { RESEND_DEV_EMAIL } from "@/lib/constants";
+
 export const ResendOTP = Email({
   id: "resend-otp",
   apiKey: process.env.RESEND_API_KEY,
@@ -14,7 +16,7 @@ export const ResendOTP = Email({
     const resend = new ResendAPI(provider.apiKey);
     const toEmail = process.env.DEV_EMAIL ?? email;
     const { error } = await resend.emails.send({
-      from: process.env.AUTH_EMAIL ?? "murmur.chat <onboarding@resend.dev>",
+      from: process.env.AUTH_EMAIL ?? RESEND_DEV_EMAIL,
       to: process.env.DEV_EMAIL ?? email,
       subject: `Sign in to murmur.chat`,
       text: "Your code is " + token,
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,2 @@
 export const INVITE_PARAM = "invite";
+export const RESEND_DEV_EMAIL = "murmur.chat <onboarding@resend.dev>";
