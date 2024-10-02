fffe0dcbe68e1d82dfdd52889b6db438c30a8ff1
Luigi Labigalini <26639149+labigalini@users.noreply.github.com>
2024-10-02 00:30:29 -0700

    Read search param invite and log

--- app/[chatId]/page.tsx
+++ app/[chatId]/page.tsx
@@ -1,6 +1,17 @@
+import { INVITE_PARAM } from "@/lib/constants";
+
 import ChatBoard from "./_components/ChatBoard";
 
-export default function Page({ params }: { params: { chatId?: string } }) {
+export default function Page({
+  params,
+  searchParams,
+}: {
+  params: { chatId?: string };
+  searchParams: { [INVITE_PARAM]?: string };
+}) {
   const { chatId } = params;
+  const invite = searchParams[INVITE_PARAM];
+  console.log({ invite });
+  // TODO open dialog to accept 
   return <ChatBoard selectedChatId={chatId} />;
 }
--- convex/invites.ts
+++ convex/invites.ts
@@ -57,7 +57,7 @@ export const get = query({
       return null;
     }
     const invite = await ctx.table("invites").getX(inviteId);
-    checkViewerWasInvited(ctx, invite);
+    checkViewerWasInvitedX(ctx, invite);
     const user = await invite.edge("user");
     return {
       _id: invite._id,
@@ -75,7 +75,7 @@ export const accept = mutation({
   },
   async handler(ctx, { inviteId }) {
     const invite = await ctx.table("invites").getX(inviteId);
-    checkViewerWasInvited(ctx, invite);
+    checkViewerWasInvitedX(ctx, invite);
     const existingMember = await ctx
       .table("members", "chatUser", (q) =>
         q.eq("chatId", invite.chatId).eq("userId", ctx.viewerX()._id),
@@ -103,7 +103,7 @@ export const deleteInvite = mutation({
   },
   async handler(ctx, { inviteId }) {
     const invite = await ctx.table("invites").getX(inviteId);
-    checkViewerWasInvited(ctx, invite);
+    checkViewerWasInvitedX(ctx, invite);
     await ctx.table("invites").getX(inviteId).delete();
   },
 });
@@ -112,7 +112,7 @@ export const deleteInvite = mutation({
 // with a different email, add a cryptographically secure token to
 // the invite ent, and use it in place of the ID as INVITE_PARAM.
 // Don't use the invite ID, as it shown to all team members.
-function checkViewerWasInvited(ctx: QueryCtx, invite: Ent<"invites">) {
+function checkViewerWasInvitedX(ctx: QueryCtx, invite: Ent<"invites">) {
   if (invite.email !== ctx.viewerX().email) {
     throw new Error("Invite email does not match viewer email");
   }
